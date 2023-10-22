a136ac43b04089904118da396a9f0208bca414cb
roshan21k <k.roshan21k@gmail.com>
2023-10-22 19:53:09 +0530

    wba test

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
 import store from "./store";
+import './registerServiceWorker'
 axios.interceptors.request.use((config) => {
   config.baseURL = "http://127.0.0.1:5000/";
   if (config.url === "auth/refresh") {
