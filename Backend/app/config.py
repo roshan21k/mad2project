9d9deefd07db2b9668a8be2e5f0d2e9036bba537
roshan21k <k.roshan21k@gmail.com>
2023-10-26 22:28:08 +0530

    add mailtrap server

--- Backend/app/config.py
+++ Backend/app/config.py
@@ -11,8 +11,12 @@ class Config:
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = int(os.getenv("MAIL_PORT"))
-    MAIL_USE_SSL = os.getenv("MAIL_USE_SSL")
+    # MAIL_USE_SSL = os.getenv("MAIL_USE_SSL")
     # MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
+    # MAIL_USE_SSL = False
+    # MAIL_USE_TLS = True
+    MAIL_USE_SSL = True
+    MAIL_USE_TLS = False
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
